d50a4db4b8a2f60b5ea71b1ed4ceef59f4322501
Maksym Yeromenko <maksimeromenko@gmail.com>
2025-03-10 21:11:26 +0100

    .gitignore updated

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ const router = createBrowserRouter([
     {
         path: '/',
         element: <Layout/>,
-        errorElement: <ErrorPage/>,
+        // errorElement: <ErrorPage/>,
 
         children: [
             {
